c0bc242455a5430dc2c97f46ff2f2379db8d4971
vanloc0301 <locha9066@gmail.com>
2015-06-12 00:43:47 +0700

    add many file saleso

---
+++ ShopDing.Web/ViewModels/SalesOrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopDing.Web.ViewModels
+{
+    public class SalesOrderViewModel
+    {
+        public int SalesOrderId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string PONumber { get; set; }
+
+        public string MessageToClient { get; set; }
+    }
+}
